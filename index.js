571986aa835996246468e9d1aa26ffac4e64c9ee
William Holcombe <william.holcombe@uconn.edu>
2018-09-06 21:45:36 -0400

    backend started

--- index.js
+++ index.js
@@ -8,6 +8,66 @@ const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
+
+
+
+
+const dog1 = {
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+};
+
+const cat1 = {
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+};
+
+const dogs = [dog1,
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Juice',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  }, 
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Deuce',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  }];
+
+const cats = [cat1, 
+  {imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Gluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'},
+  {imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Shleppy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'}];
+
 const app = express();
 
 app.use(
@@ -21,6 +81,36 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+
+app.get('/api/cat', (req, res, next) => {
+
+  res.json(cats[0]);
+
+});
+
+
+app.delete('/api/cat', (req, res, next) => {
+
+  cats.splice(0, 1); 
+  res.sendStatus(204);
+   
+
+});
+
+app.get('/api/dog', (req, res, next) => {
+
+  res.json(dogs[0]);
+
+});
+
+app.delete('/api/dog', (req, res, next) => {
+
+  dogs.splice(0, 1); 
+  res.sendStatus(204);
+
+});
+
+
 
 function runServer(port = PORT) {
   const server = app
